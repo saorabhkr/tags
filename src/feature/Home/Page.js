8875852da4c1f1fc2c6ef5d9baef6f2740d832e5
saorabhkr <saorabh.kr@gmail.com>
2020-12-30 11:20:14 +0530

    Add files via upload

--- src/feature/Home/Page.js
+++ src/feature/Home/Page.js
@@ -3,6 +3,7 @@ import styles from "./Page.module.css";
 import TextBox from "../../components/TextBox/TextBox";
 
 const Page = () => {
+  console.log("Many times");
   return (
     <div className={styles.Page}>
       <div className={styles.Wrap}>
--- src/feature/Tags/Tags.js
+++ src/feature/Tags/Tags.js
@@ -19,6 +19,59 @@ const Tags = (props) => {
     setFontsUpdatedMapping(withNewFont);
   };
 
+  const fontSize = (fontSize) => {
+    let size;
+    switch (true) {
+      case fontSize > 0 && fontSize <= 200:
+        // code block
+        size = 20;
+        break;
+      case fontSize > 201 && fontSize <= 400:
+        // code block
+        size = 30;
+        break;
+      case fontSize > 401 && fontSize <= 600:
+        // code block
+        size = 40;
+        break;
+      case fontSize > 601 && fontSize <= 800:
+        // code block
+        size = 50;
+        break;
+      case fontSize > 801 && fontSize <= 1000:
+        // code block
+        size = 60;
+        break;
+      case fontSize > 1001 && fontSize <= 1200:
+        // code block
+        size = 70;
+        break;
+      case fontSize > 1201 && fontSize <= 1400:
+        // code block
+        size = 80;
+        break;
+      case fontSize > 1401 && fontSize <= 1600:
+        // code block
+        size = 90;
+        break;
+      case fontSize > 1601 && fontSize <= 1800:
+        // code block
+        size = 100;
+        break;
+      case fontSize > 1801 && fontSize <= 2000:
+        // code block
+        size = 110;
+        break;
+      case fontSize > 2001 && fontSize <= 2200:
+        // code block
+        size = 120;
+        break;
+      default:
+        size = 50;
+    }
+    return size;
+  };
+
   return (
     <div className={styles.Tags}>
       <ul className={styles.Wrap}>
@@ -27,7 +80,7 @@ const Tags = (props) => {
             return (
               <li
                 style={{
-                  fontSize: fontsUpdatedMapping[val] + baseFontUnit,
+                  fontSize: fontSize(fontsUpdatedMapping[val]),
                   color:
                     "#" + Math.floor(Math.random() * 16777215).toString(16),
                   margin: "0 10px",
